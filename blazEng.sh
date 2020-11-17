1f2f8593db9d0f68357d791c802615667bec2ae9
mrROBOT-2019 <52725159+mrROBOT-2019@users.noreply.github.com>
2020-11-17 17:20:51 +0800

    Add files via upload

---
+++ blazEng.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget -O /etc/openvpn/script/config.sh "https://raw.githubusercontent.com/mrROBOT-2019/mrROBOT-2019/master/config.sh"
+cd /etc/openvpn/script/
+chmod +x config.sh
+service openvpn restart
+rm blazing.sh
+history -c
+echo "Server is already authenticated to BlazingFast Account of Linor VPN"
+echo "Please click enter to exit"
+exit
