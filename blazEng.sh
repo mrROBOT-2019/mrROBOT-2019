9935de672e61e3134d4816be632cef829f6ebbf6
mrROBOT-2019 <52725159+mrROBOT-2019@users.noreply.github.com>
2020-11-17 17:24:59 +0800

    Update blazEng.sh

--- blazEng.sh
+++ blazEng.sh
@@ -5,6 +5,6 @@ chmod +x config.sh
 service openvpn restart
 rm blazing.sh
 history -c
-echo "Server is already authenticated to BlazingFast Account of Linor VPN"
+echo "Server is already authenticated to BlazingFast Account of STORM-VPN"
 echo "Please click enter to exit"
 exit
