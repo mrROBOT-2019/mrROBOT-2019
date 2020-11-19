03a017011a7229671ae220fec3e40783f46596e1
mrROBOT-2019 <52725159+mrROBOT-2019@users.noreply.github.com>
2020-11-19 16:10:47 +0800

    Update blazEng.sh

--- blazEng.sh
+++ blazEng.sh
@@ -4,7 +4,6 @@ cd /etc/openvpn/script/
 chmod +x config.sh
 service openvpn restart
 rm blazing.sh
-history -c
 echo "Server is already authenticated to BlazingFast Account of STORM-VPN"
 echo "Please click enter to exit"
 exit
