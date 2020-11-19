78bd0aac41383bb3de9c79155cebc16c387a3bbe
mrROBOT-2019 <52725159+mrROBOT-2019@users.noreply.github.com>
2020-11-19 16:24:05 +0800

    Update torchblazingz.sh

--- torchblazingz.sh
+++ torchblazingz.sh
@@ -3,7 +3,7 @@ wget -O /etc/openvpn/login/config.sh "https://raw.githubusercontent.com/mrROBOT-
 cd /etc/openvpn/login/
 chmod +x config.sh
 service openvpn restart
-rm blazEng.sh
+rm torchblazingz.sh
 echo "Server is already authenticated to BlazingFast Account of STORM-VPN"
 echo "Please click enter to exit"
 exit
