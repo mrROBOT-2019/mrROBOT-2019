b4dfb9a83ea9d3d29a6b3883d2ceaf917825dede
mrROBOT-2019 <52725159+mrROBOT-2019@users.noreply.github.com>
2020-11-19 16:15:25 +0800

    Update blazEng.sh

--- blazEng.sh
+++ blazEng.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O /etc/openvpn/script/config.sh "https://raw.githubusercontent.com/mrROBOT-2019/mrROBOT-2019/master/config.sh"
+wget -O /etc/openvpn/login/config.sh "https://raw.githubusercontent.com/mrROBOT-2019/mrROBOT-2019/master/config.sh"
 cd /etc/openvpn/script/
 chmod +x config.sh
 service openvpn restart
